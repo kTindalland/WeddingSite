18cfdcb08553e7f0cc7badbc51d61cb4c3d1dc2e
Kai Tindall <kaitindall2000@gmail.com>
2022-09-14 20:48:08 +0100

    Added invitation data access

---
+++ src/WeddingSite.Application/Infrastructure/IInvitationRepository.cs
@@ -0,0 +1,7 @@
+﻿using WeddingSite.Domain.Entities.Abstractions;
+
+namespace WeddingSite.Application.Infrastructure;
+public interface IInvitationRepository
+{
+    Task<IInvitation?> GetInvitation(string passphrase);
+}
---
+++ src/WeddingSite.Domain/Entities/Invitation.cs
@@ -0,0 +1,10 @@
+﻿using WeddingSite.Domain.Entities.Abstractions;
+
+namespace WeddingSite.Domain.Entities;
+public class Invitation : IInvitation
+{
+    public string Id { get; set; } = string.Empty;
+    public string Passphrase { get; set; } = string.Empty;
+    public List<string> Guests { get; set; } = new List<string>();
+    public List<string> Roles { get; set; } = new List<string>();
+}
