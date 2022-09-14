920e2de18b24d2b1d15856eda988660906af9b7d
Kai Tindall <kaitindall2000@gmail.com>
2022-09-14 20:21:02 +0100

    Added basic endpoint

---
+++ src/WeddingSite.Server/Endpoints/AuthenticationEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Convey.CQRS.Queries;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeddingSite.Server.Endpoints;
+public static class AuthenticationEndpoints
+{
+    public static WebApplication MapAuthenticationEndpoints(this WebApplication app)
+    {
+
+        app.MapPost("api/auth/get-token", GetAuthToken)
+            .Produces(200)
+            .WithName("Get Token")
+            .WithTags("Authentication");
+
+        return app;
+    }
+
+    private static Task<IResult> GetAuthToken(
+        [FromServices] IQueryDispatcher queryDispatcher,
+        [FromBody] string passphrase)
+    {
+
+
+        return Task.FromResult(Results.Ok(passphrase));
+    }
+}
