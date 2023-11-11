def3c072e29bfa451541f4f510984e1ac4943a04
Kai Tindall <kaitindall2000@gmail.com>
2023-11-11 21:30:02 +0000

    Implemented the attendance indicator v1

---
+++ src/WeddingSite.Client/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace WeddingSite.Client.Extensions;
+
+public static class StringExtensions
+{
+    private static readonly string[] SortOrder = {
+        "attendance",
+        "food"
+    };
+    
+    public static int IndicatorPosition(this string section)
+    {
+        var order = Array.IndexOf(SortOrder, section);
+
+        return order == -1
+            ? int.MaxValue
+            : order;
+    }
+}
